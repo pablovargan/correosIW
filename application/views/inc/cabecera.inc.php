5001548a2ca30441d657ab0f27357d3703568732
pvr3 <pvr3@alu.ua.es>
2014-01-08 11:38:42 +0100

    Css

--- application/views/inc/cabecera.inc.php
+++ application/views/inc/cabecera.inc.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php echo $tituloHead ?></title>
+	<link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css" type="text/css" media="screen"/>
+	<link rel="stylesheet" href="<?php echo base_url();?>css/sb-admin.css" type="text/css" media="screen"/>
 </head>
 <body>
 
